0e68f25b705b191da0bd6a1fc0901322318091b9
geby9469 <bjk9406@gmail.com>
2025-04-03 15:44:55 +0900

    update legacy libraries to modern alternatives

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # AWS KMS based Ethereum Tx Signing
-This repo shows how to sign an Ethereum transaction using AWS KMS. 
+
+This repo shows how to sign an Ethereum transaction using AWS KMS.
+
+## Migration Updates
+
+Migrated from legacy libraries to modern equivalents:
+
+1. Replaced bn, web3, and ethereum-utils with ethers.js v6
+2. Upgraded from AWS SDK v2 to AWS SDK v3
+3. Switched from Kovan (Infura) to Sepolia (Alchemy) for Ethereum testnet services
+
+Successful transaction: [View on Etherscan](https://sepolia.etherscan.io/tx/0xa5dcebd1abc37d76d072b6a8442f8a363569e217ab6069a576be96a5e74000d2)
 
 ## Medium
-Please see my [medium article](https://luhenning.medium.com/the-dark-side-of-the-elliptic-curve-signing-ethereum-transactions-with-aws-kms-in-javascript-83610d9a6f81) for a detailed code walk-through. 
+
+Please see my [medium article](https://luhenning.medium.com/the-dark-side-of-the-elliptic-curve-signing-ethereum-transactions-with-aws-kms-in-javascript-83610d9a6f81) for a detailed code walk-through.
 
 ## Prep
+
 1. Create ECDSA secp256k1 key in AWS KMS
 2. Create AWS IAM user with programmatic access to AWS KMS.
-3. For the Tx to go through, you need to provide a valid web3 provider (e.g. Infura)
+3. For the Tx to go through, you need to provide a valid web3 provider (e.g. Alchemy)
 4. Run the script to generate the Ethereum address and fund the Ethereum account to pay for the transaction gas.
--- aws-kms-sign.ts
+++ aws-kms-sign.ts
@@ -1,32 +1,28 @@
-import { KMS } from 'aws-sdk';
-import { keccak256 } from 'js-sha3';
-import * as ethutil from 'ethereumjs-util';
-import Web3 from 'web3';
 import * as asn1 from 'asn1.js';
-import BN from 'bn.js';
-import { Transaction, TxData } from 'ethereumjs-tx';
-import { TransactionReceipt } from 'web3-core/types';
+import { GetPublicKeyCommand, KMSClient, SignCommand } from '@aws-sdk/client-kms';
+import { computeAddress, JsonRpcProvider, parseUnits, SigningKey, Transaction, keccak256 } from 'ethers';
 
-const kms = new KMS({
-    accessKeyId: '<access_key_id>', // credentials for your IAM user with KMS access
-    secretAccessKey: '<access_secret>', // credentials for your IAM user with KMS access
-    region: 'us-east-1',
-    apiVersion: '2014-11-01',
+const kms = new KMSClient({
+    credentials: {
+        accessKeyId: '<access_key_id>',
+        secretAccessKey: '<access_secret>',
+    },
+    region: '<region>',
 });
 
 const keyId = '<KMS key id>';
 
 const EcdsaSigAsnParse = asn1.define('EcdsaSig', function(this: any) {
-    // parsing this according to https://tools.ietf.org/html/rfc3279#section-2.2.3 
-    this.seq().obj( 
-        this.key('r').int(), 
+    // parsing this according to https://tools.ietf.org/html/rfc3279#section-2.2.3
+    this.seq().obj(
+        this.key('r').int(),
         this.key('s').int(),
     );
 });
 
 const EcdsaPubKey = asn1.define('EcdsaPubKey', function(this: any) {
     // parsing this according to https://tools.ietf.org/html/rfc5480#section-2
-    this.seq().obj( 
+    this.seq().obj(
         this.key('algo').seq().obj(
             this.key('a').objid(),
             this.key('b').objid(),
@@ -36,148 +32,185 @@ const EcdsaPubKey = asn1.define('EcdsaPubKey', function(this: any) {
 });
 
 async function sign(msgHash, keyId) {
-    const params : KMS.SignRequest = {
-        // key id or 'Alias/<alias>'
-        KeyId: keyId, 
-        Message: msgHash, 
-        // 'ECDSA_SHA_256' is the one compatible with ECC_SECG_P256K1.
+    // For more information, see https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/kms/command/SignCommand
+    const signCommand = new SignCommand({
+        KeyId: keyId,
+        Message: hexStringToUint8Array(msgHash),
+        MessageType: 'DIGEST',
         SigningAlgorithm: 'ECDSA_SHA_256',
-        MessageType: 'DIGEST' 
-    };
-    const res = await kms.sign(params).promise();
-    return res;
+    });
+    return await kms.send(signCommand);
 }
 
 async function getPublicKey(keyPairId: string) {
-    return kms.getPublicKey({
-        KeyId: keyPairId
-    }).promise();
+    const command = new GetPublicKeyCommand({
+        KeyId: keyPairId,
+    });
+    return await kms.send(command);
 }
 
-function getEthereumAddress(publicKey: Buffer): string {
-    console.log("Encoded Pub Key: " + publicKey.toString('hex'));
-
-    // The public key is ASN1 encoded in a format according to 
+function getEthereumAddress(publicKey: Uint8Array): string {
+    if(publicKey === undefined) {
+        throw new Error('Public key is undefined.');
+    }
+    // The public key is ASN1 encoded in a format according to
     // https://tools.ietf.org/html/rfc5480#section-2
-    // I used https://lapo.it/asn1js to figure out how to parse this 
+    // I used https://lapo.it/asn1js to figure out how to parse this
     // and defined the schema in the EcdsaPubKey object
-    let res = EcdsaPubKey.decode(publicKey, 'der');
+    let res = EcdsaPubKey.decode(Buffer.from(publicKey), 'der')
     let pubKeyBuffer : Buffer = res.pubKey.data;
 
     // The public key starts with a 0x04 prefix that needs to be removed
     // more info: https://www.oreilly.com/library/view/mastering-ethereum/9781491971932/ch04.html
-    pubKeyBuffer = pubKeyBuffer.slice(1, pubKeyBuffer.length);
-
-    const address = keccak256(pubKeyBuffer) // keccak256 hash of publicKey  
-    const buf2 = Buffer.from(address, 'hex');
-    const EthAddr = "0x" + buf2.slice(-20).toString('hex'); // take last 20 bytes as ethereum adress
+    // But you can just convert to hex string because ethers.js will do the conversion for you.
+    const EthAddr = computeAddress('0x' +pubKeyBuffer.toString('hex'));
     console.log("Generated Ethreum address: " + EthAddr);
     return EthAddr;
 }
 
-async function findEthereumSig(plaintext) {
-    let signature = await sign(plaintext, keyId);
+async function findEthereumSig(msgHash) {
+    let signature = await sign(msgHash, keyId);
     if (signature.Signature == undefined) {
         throw new Error('Signature is undefined.');
     }
-    console.log("encoded sig: " + signature.Signature.toString('hex'));
 
-    let decoded = EcdsaSigAsnParse.decode(signature.Signature, 'der');
-    let r : BN = decoded.r;
-    let s : BN = decoded.s;
-    console.log("r: " + r.toString(10));
-    console.log("s: " + s.toString(10));
+    let decoded = EcdsaSigAsnParse.decode(Buffer.from(signature.Signature), 'der');
+    let r: bigint = decoded.r;
+    let s: bigint = decoded.s;
 
-    let tempsig = r.toString(16) + s.toString(16);
+    // convert to decimal
+    console.log("r: " + r);
+    console.log("s: " + s);
 
-    let secp256k1N = new BN("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16); // max value on the curve
-    let secp256k1halfN = secp256k1N.div(new BN(2)); // half of the curve
+    let tempsig = decoded.r.toString(16) + decoded.s.toString(16);
+    console.log(tempsig);
+
+    const secp256k1N: bigint = BigInt(
+        '0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141'
+    );
+    const secp256k1halfN = secp256k1N / BigInt(2);
+
     // Because of EIP-2 not all elliptic curve signatures are accepted
     // the value of s needs to be SMALLER than half of the curve
     // i.e. we need to flip s if it's greater than half of the curve
-    if (s.gt(secp256k1halfN)) {
+    if (s > secp256k1halfN) {
         console.log("s is on the wrong side of the curve... flipping - tempsig: " + tempsig + " length: " + tempsig.length);
         // According to EIP2 https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2.md
-        // if s < half the curve we need to invert it 
+        // if s < half the curve we need to invert it
         // s = curve.n - s
-        s = secp256k1N.sub(s);
-        console.log("new s: " + s.toString(10));
-        return { r, s }
+        s = secp256k1N - BigInt(s);
     }
     // if s is less than half of the curve, we're on the "good" side of the curve, we can just return
     return { r, s }
 }
 
-function recoverPubKeyFromSig(msg: Buffer, r : BN, s : BN, v: number) {
-    console.log("Recovering public key with msg " + msg.toString('hex') + " r: " + r.toString(16) + " s: " + s.toString(16));
-    let rBuffer = r.toBuffer();
-    let sBuffer = s.toBuffer();
-    let pubKey = ethutil.ecrecover(msg, v, rBuffer, sBuffer);
-    let addrBuf = ethutil.pubToAddress(pubKey);
-    var RecoveredEthAddr = ethutil.bufferToHex(addrBuf);
-    console.log( "Recovered ethereum address: " +  RecoveredEthAddr);
-    return RecoveredEthAddr;
-}
-
-function findRightKey(msg: Buffer, r : BN, s: BN, expectedEthAddr: string) {
+function findRightKey(msg: string, r, s, expectedEthAddr: string) {
     // This is the wrapper function to find the right v value
     // There are two matching signatues on the elliptic curve
     // we need to find the one that matches to our public key
     // it can be v = 27 or v = 28
-    let v = 27;
-    let pubKey = recoverPubKeyFromSig(msg, r, s, v);
+    let vf = 27;
+    let pubKey = SigningKey.recoverPublicKey(
+        msg,
+        {
+            r: '0x' + r.toString(16),
+            s: '0x' + s.toString(16),
+            v: vf,
+        }
+    );
+
+    pubKey = computeAddress(pubKey);
     if (pubKey != expectedEthAddr) {
         // if the pub key for v = 27 does not match
         // it has to be v = 28
-        v = 28;
-        pubKey = recoverPubKeyFromSig(msg, r, s, v)
+        vf = 28;
+        pubKey = SigningKey.recoverPublicKey(
+            msg,
+            {
+                r: '0x' + r.toString(16),
+                s: '0x' + s.toString(16),
+                v: vf,
+            }
+        );
+        pubKey = computeAddress(pubKey);
     }
-    console.log("Found the right ETH Address: " + pubKey + " v: " + v);
-    return { pubKey, v };
+    console.log("Found the right ETH Address: " + pubKey + " v: " + vf);
+    return { pubKey, vf };
+}
+
+function hexStringToUint8Array(hex: string): Uint8Array {
+    if (hex.startsWith('0x')) hex = hex.slice(2);
+    if (hex.length % 2 !== 0) throw new Error('Invalid hex string');
+
+    const bytes = new Uint8Array(hex.length / 2);
+    for (let i = 0; i < hex.length; i += 2) {
+        bytes[i / 2] = parseInt(hex.slice(i, i + 2), 16);
+    }
+    return bytes;
 }
 
 txTest();
 async function txTest() {
-    const web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/v3/<infura_key>"));
+    console.log('test');
+    // Use a provider using ethers.js instead of web3.js
+    const provider = new JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/<alchemy_key>');
 
     let pubKey = await getPublicKey(keyId);
-    let ethAddr = getEthereumAddress((pubKey.PublicKey as Buffer));
-    let ethAddrHash = ethutil.keccak(Buffer.from(ethAddr));
-    let sig = await findEthereumSig(ethAddrHash);
-    let recoveredPubAddr = findRightKey(ethAddrHash, sig.r, sig.s, ethAddr);
+    let ethAddr = getEthereumAddress(pubKey.PublicKey ?? new Uint8Array());;
 
-    const txParams: TxData = {
-        nonce: await web3.eth.getTransactionCount(ethAddr),
-        gasPrice: '0x0918400000',
-        gasLimit: 160000,
-        to: '0x0000000000000000000000000000000000000000',
-        value: '0x00',
-        data: '0x00',
-        r: sig.r.toBuffer(),
-        s: sig.s.toBuffer(),
-        v: recoveredPubAddr.v
+    const receiverAddr = '<receiver_address>';
+    const etherValue = parseUnits('1000000', 'gwei'); // 1,000,000 gwei = 0.001 ether
+    const chainId = (await provider.getNetwork()).chainId;
+    const nonce = await provider.getTransactionCount(ethAddr);
+    let gasLimit = await provider.estimateGas({
+        from: ethAddr,
+        to: receiverAddr,
+        nonce: nonce,
+        value: etherValue,
+        chainId: chainId,
+    });
+    const feeData = await provider.getFeeData();
+    // For more informations, https://docs.ethers.org/v6/api/providers/#FeeData
+    const maxFeePerGas = feeData.maxFeePerGas ? (feeData.maxFeePerGas * BigInt(3)) / BigInt(2) : parseUnits("50", "gwei");
+    const maxPriorityFeePerGas = feeData.maxPriorityFeePerGas ? feeData.maxPriorityFeePerGas : parseUnits("2", "gwei");
+
+    // Type 2 (EIP-1559) transaction params
+    const tx = new Transaction();
+    tx.to = receiverAddr;
+    tx.nonce = nonce;
+    tx.value = etherValue;
+    tx.chainId = chainId;
+    tx.maxFeePerGas = maxFeePerGas;
+    tx.maxPriorityFeePerGas = maxPriorityFeePerGas;
+    tx.gasLimit = gasLimit;
+
+    console.log(tx.toJSON());
+
+    const txHash = keccak256(tx.unsignedSerialized);
+    const sig = await findEthereumSig(txHash);
+    const recoveredPubAddr = findRightKey(txHash, sig.r, sig.s, ethAddr);
+
+    // Adjust the v value considering EIP-155
+    // For more infomation, see {https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md}
+    const chainIdV = chainId * BigInt(2) + BigInt(35);
+    const v = recoveredPubAddr.vf === 27 ? chainIdV : chainIdV + BigInt(1);
+
+    tx.signature = {
+        r: '0x' + sig.r.toString(16),
+        s: '0x' + sig.s.toString(16),
+        v: v,
+    };
+    console.log(tx.from);
+    if(tx.from === recoveredPubAddr.pubKey) {
+        console.log('Transaction from address matches the recovered address');
     }
 
-    console.log(txParams);
+    // Send signed tx to ethereum network
+    const txResponse = await provider.broadcastTransaction(tx.serialized);
+    const txReceipt = await txResponse.wait();
+    console.log("signAndSendTx txReceipt. Tx Hash: " + txReceipt?.hash);
 
-    const tx = new Transaction(txParams, {
-        chain: 'kovan',
-    });
-
-    let txHash = tx.hash(false);
-    sig = await findEthereumSig(txHash);
-    recoveredPubAddr = findRightKey(txHash, sig.r, sig.s, ethAddr);
-    tx.r = sig.r.toBuffer();
-    tx.s = sig.s.toBuffer();
-    tx.v = new BN(recoveredPubAddr.v).toBuffer();
-    console.log(tx.getSenderAddress().toString('hex'));
-
-    // Send signed tx to ethereum network 
-    const serializedTx = tx.serialize().toString('hex');    
-    web3.eth.sendSignedTransaction('0x' + serializedTx)
-    .on('confirmation', (confirmationNumber : number, receipt : TransactionReceipt) => {})
-    .on('receipt', (txReceipt : TransactionReceipt) => { 
-        console.log("signAndSendTx txReceipt. Tx Address: " + txReceipt.transactionHash);
-    })  
-    .on('error', error => console.log(error));
+    return {
+        success: true,
+    }
 }
